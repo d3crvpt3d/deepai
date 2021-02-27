e8888baf4f1877052a3ab5053407a5af40a6981e
phibr0 <bronzel.phillip@gmail.com>
2021-02-27 19:18:47 +0100

    unzip the rar because why the fuck should you do it in a rar

---
+++ 1_get_rgba_values.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+im = Image.open('Images/original_input.png', 'r')
+width, height = im.size
+pixel_values = list(im.getdata())
+
+print(im.mode)
+
+x=0
+y=0
+
+done=False
+
+file = open("tmp/test_output.ai_rgba","w")
+
+tmp_values =''
+
+while(done!=True):
+    tmp_values = tmp_values + str(pixel_values[x+y])
+    x=x+1
+    if(x>=width):
+        x=0
+        file.write( tmp_values+'\n' )
+        y=y+1
+        tmp_values = ''
+        if(y>=height):
+            done=True
+
+file.close
+
+print("done")
